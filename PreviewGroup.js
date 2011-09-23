740a174c693e17b0ae1b152560338f3303069c6e
leigh.pauls <leigh.pauls@localhost>
2011-09-23 17:17:29 -0400

    missed some files in last commit

---
+++ PreviewGroup.js
@@ -0,0 +1,40 @@
+
+function PreviewGroup(baseX, baseY) {
+    var i;
+    
+    this.blocks = [];
+
+    // create the blocks
+    for (i = 0; i < 4; i++) {
+	this.blocks.push(new Block({
+	    boardOriginX: baseX,
+	    boardOriginY: baseY,
+	    blockX: 0,
+	    blockY: 0,
+	    shape: 'i'
+	}));
+    }
+}
+
+/**
+* Sets the shape and color of the blocks
+* @param {Char} shape - the letter of the new shape
+* @param {Boolean} preview - true if it should have preview colors
+*/
+PreviewGroup.prototype.setShape = function(shape) {
+    var shapeConfig = SHAPES[shape],
+    i;
+    
+    for (i = 0; i < 4; i++) {
+	this.blocks[i].setPosition(shapeConfig.pos[i].x, shapeConfig.pos[i].y);
+	this.blocks[i].setColor(shape, false);
+    }
+}
+
+PreviewGroup.prototype.draw = function() {
+    var i;
+    for (i = 0; i < 4; i++) {
+	this.blocks[i].draw();
+    }
+}
+				
