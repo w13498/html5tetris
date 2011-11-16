a8c190615fc8dc15ad6dc2d0f4a890c8ac14fca7
leigh <leigh@localhost>
2011-11-16 12:05:56 -0500

    changed the file structure and makefile to be a little more managable

---
+++ src/js/controls.js
@@ -0,0 +1,8 @@
+function onControlsLoad() {
+    // check for an existing controls cookie
+    
+    // if no cookie, assign defaults, create the cookie
+    
+    // if there is a cookie, set up the controls for it
+}
+
